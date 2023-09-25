9a35d38153851c8e0a0c2cc7ac30b2f9fe50738c
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-09-25 16:13:31 -0500

    LoginPage.dart

--- lib/app/view/login/login_page.dart
+++ lib/app/view/login/login_page.dart
@@ -1,27 +1,139 @@
 import 'package:flutter/material.dart';
-import '../welcome_page/welcome_page.dart';// Asegúrate de importar WelcomePage correctamente
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  bool _isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
+        elevation: 0,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           color: Colors.black,
           onPressed: () {
-            // Navegar de regreso a WelcomePage al hacer clic en la flecha
             Navigator.pop(context);
           },
         ),
-        title: const Text(
-          'Login',
-          style: TextStyle(color: Colors.black),
-        ),
+        title: const Text(''),
       ),
-      body: const Center(
-        child: Text('página de inicio de sesión'),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+            child: Row(
+              children: [
+                Text(
+                  'Inicia Sesión',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+            child: Row(
+              children: [
+                const Text(
+                  'Wau Walk',
+                  style: TextStyle(
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Spacer(),
+                Image.asset(
+                  'assets/logo_sm_wauwalk.png',
+                  height: 50.0,
+                  width: 50.0,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              color: const Color(0xFFE7ECEF),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Usuario',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Contraseña',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        _isChecked = !_isChecked;
+                      });
+                    },
+                    child: Row(
+                      children: [
+                        Checkbox(
+                          value: _isChecked,
+                          onChanged: (value) {
+                            setState(() {
+                              _isChecked = value!;
+                            });
+                          },
+                        ),
+                        const Text('Recorder mi usuario y contraseña'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        onPrimary: Colors.black,
+                      ),
+                      onPressed: () {
+                        // Handle login button press
+                      },
+                      child: const Text('Iniciar sesión'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
