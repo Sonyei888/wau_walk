a1ad8eef8b69a3ab0fd1191fb929b2b058900bc3
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-10-11 22:58:28 -0500

    Implementacion SplashPage v2

--- lib/app/view/splash/splash_page.dart
+++ lib/app/view/splash/splash_page.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wau_walk/app/view/welcome_page/welcome_page.dart';
-import 'dart:async';
 
-class SplashPage extends StatefulWidget {
-  @override
-  _SplashPageState createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-
-    // Espera 3 segundos y luego navega a WelcomePage
-    Timer(Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const WelcomePage()),
-      );
-    });
-  }
-
+class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +16,7 @@ class _SplashPageState extends State<SplashPage> {
             ),
             const SizedBox(height: 100),
             const CircularProgressIndicator(
+
               valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
             ),
           ],
