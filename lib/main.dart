244da10c8dccb8837db489179ecb187c27432691
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-10-21 00:54:37 -0500

    Implementacion setPreferredOrientations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wau_walk/app/app.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
